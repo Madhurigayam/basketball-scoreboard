c782c9c8d3fceeaae0c2b65e423166277b228790
Madhurigayam <156207778+Madhurigayam@users.noreply.github.com>
2024-04-20 20:48:16 +0530

    Add files via upload

---
+++ score.js
@@ -0,0 +1,26 @@
+let counth=0
+let countg=0
+function one(){
+    counth+=1
+    document.getElementById("homepts").textContent=counth
+}
+function two(){
+    counth+=2
+    document.getElementById("homepts").textContent=counth
+}
+function three(){
+    counth+=3
+    document.getElementById("homepts").textContent=counth
+}
+function oneb(){
+    countg+=1
+    document.getElementById("guestpts").textContent=countg
+}
+function twob(){
+    countg+=2
+    document.getElementById("guestpts").textContent=countg
+}
+function threeb(){
+    countg+=3
+    document.getElementById("guestpts").textContent=countg
+}
